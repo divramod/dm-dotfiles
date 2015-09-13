e7d07c29f7f0eac6c90cf2046e01a13709ebddf2
divramod <divramod@gmail.de>
2015-09-13 16:31:50 +0200

    added vimLinkSnipptets

--- tasks/vimLinkSnippets/index.js
+++ tasks/vimLinkSnippets/index.js
@@ -1,15 +1,47 @@
 // =========== [ REQUIRE ] ===========
 var co = require("co");
+var dmPrompt = require("dm-prompt").Inquirer;
+var dmPath = require("dm-path");
+var colors = require("colors");
+var path = require("path");
+var spawn = require("dm-shell").spawn;
+require('shelljs/global');
+
 
 // =========== [ MODULE DEFINE ] ===========
 var task = {};
 
 // =========== [ SYNC task.start() ] ===========
-task.start = function(p1) {
+task.start = function() {
     try {
-        process.env.debug = false; // for debugging purposes
-        var p1 = p1 || process.argv[3] || undefined;
-        return run(p1);
+        var snippetsPath = dmPath.replace("~/dotfiles/vim/snippets");
+        var bundleSnippetsPath = dmPath.replace("~/dotfiles/vim/vim/bundle/vim-snippets/snippets");
+        if (test("-d", snippetsPath) && test("-d", bundleSnippetsPath)) {
+
+            var mySnippets = ls(snippetsPath);
+            for (var i = 0, l = mySnippets.length; i < l; i++) {
+                var s = mySnippets[i];
+                // create directory if not existing
+                var pathLanguage = path.resolve(bundleSnippetsPath, s);
+                if (!test("-d", pathLanguage)) {
+                    mkdir(pathLanguage);
+                }
+
+                // link files
+                var files = ls(path.resolve(snippetsPath, s));
+                for (var j = 0, ll = files.length; j < ll; j++) {
+                    var f = files[j];
+                    var src = path.resolve(snippetsPath, s, f);
+                    var dest = path.resolve(bundleSnippetsPath, s, f);
+                    var command = "ln -s " + src + " " + dest;
+                    console.log(command);
+                    spawn(command);
+                }
+            }
+        } else {
+            var message = snippetsPath.red + " or " + bundleSnippetsPath.red + " does not exist";
+            console.log(message);
+        }
     } catch (e) {
         if (process.env.debug === "true") {
             console.log("Filename: ", __filename, "\n", e.stack);
@@ -17,17 +49,6 @@ task.start = function(p1) {
         return e;
     }
 }; // task.start()
-
-// =========== [ SYNC run ] ===========
-var run = function(p1) {
-    try {
-        console.log("start vimLinkSnippets");
-        return p1;
-    } catch (e) {
-        console.log("Filename: ", __filename, "\n", e.stack);
-        return e;
-    }
-}; // run
 
 // =========== [ MODULE EXPORT ] ===========
 module.exports = task;
